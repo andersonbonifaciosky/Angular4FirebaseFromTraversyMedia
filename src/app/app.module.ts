2aa3cda0c36fb18839e9fcdd4dcdc7634117907d
jonathan goroza <jonathan.g.goroza@accenture.com>
2017-08-04 15:17:40 +0800

    firebase angular4 sample

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,66 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { environment } from '../environments/environment';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { HomeComponent } from './components/home/home.component';
+import { ListingsComponent } from './components/listings/listings.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ListingComponent } from './components/listing/listing.component';
+import { AddListingComponent } from './components/add-listing/add-listing.component';
+import { EditListingComponent } from './components/edit-listing/edit-listing.component';
+import {FirebaseService} from './services/firebase.service';
+import { FlashMessagesModule } from 'angular2-flash-messages';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'listings',
+    component: ListingsComponent
+  },
+  {
+    path: 'add-listing',
+    component: AddListingComponent
+  },
+  {
+    path: 'listing/:id',
+    component: ListingComponent
+  },
+  {
+    path: 'edit-listing/:id',
+    component: EditListingComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ListingsComponent,
+    NavbarComponent,
+    ListingComponent,
+    AddListingComponent,
+    EditListingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    RouterModule.forRoot(appRoutes),
+    AngularFireAuthModule,
+    FlashMessagesModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [FirebaseService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyA2f85A5HyIfaaT-2nwnx9hMSWjw9GVX-4',
+    authDomain: 'proplisting-7a236.firebaseapp.com',
+    databaseURL: 'https://proplisting-7a236.firebaseio.com',
+    projectId: 'proplisting-7a236',
+    storageBucket: 'proplisting-7a236.appspot.com',
+    messagingSenderId: '460773888948'
+  }
 };
